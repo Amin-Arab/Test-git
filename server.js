dc07212f3bd4b87ae299ff26190ebfb64d36bf6d
amin-test <aminarab0910@gmail.com>
2023-08-27 13:42:41 -0700

    this is my first try in git

---
+++ server.js
@@ -0,0 +1,31 @@
+
+/*/onst http = require("http"); 
+
+
+const  Server = http.createServer((req ,  res)=>{
+    
+    console.log (req.method);
+    console.log(req.url);
+    res.write("welcome amoooo");
+    res.end();
+});
+
+Server.listen(4000 , () => {
+    console.log("hi");
+});*/
+
+//==============================================================
+
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    console.log(req.method);
+    res.write("hiiii");
+    res.end();
+})
+
+
+server.listen(4000 ,() => {
+   console.log("start server 4000") 
+})
