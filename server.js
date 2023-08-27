fd0c3b6f351ea34271ea256bd96ecd92d4de09a0
amin-test <aminarab0910@gmail.com>
2023-08-27 14:16:47 -0700

    this test bransh

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     console.log(req.method);
-    res.write("hiciii");
+    res.write("hissciii");
     res.end();
 })
 
